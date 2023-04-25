75b4f6d227ad2d208a81f6a1896f8413b948661f
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-04-25 15:10:26 +0800

    Done with two pointers.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var nums = new[] { -7, -3, 2, 3, 11 };
+            Console.WriteLine(string.Join(",", nums));
+            var sortedSquares = SortedSquares(nums);
+            Console.WriteLine(string.Join(",", sortedSquares));
+        }
+
+        static int[] SortedSquares(int[] nums)
+        {
+            var c = nums.Length;
+            var ans = new int[c];
+            var left = 0;
+            var right = c - 1;
+
+            for (var i = c - 1; i >= 0; i--)
+            {
+                int square;
+                if (Math.Abs(nums[left]) > Math.Abs(nums[right]))
+                {
+                    square = nums[left] * nums[left];
+                    left++;
+                }
+                else
+                {
+                    square = nums[right] * nums[right];
+                    right--;
+                }
+                ans[i] = square;
+            }
+            
+            return ans;
         }
     }
 }
