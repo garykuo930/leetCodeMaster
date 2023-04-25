30fc4f36344205e27a3e9f549b9fec45a5b79f93
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-04-25 19:02:48 +0800

    Done with two pointers.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var nums = new[] { 0, 1, 0, 3, 12 };
+            MoveZeroes(nums);
+            Console.WriteLine(string.Join("", nums));
+        }
+        
+        static void MoveZeroes(int[] nums)
+        {
+            if (nums.Length == 1) return;
+        
+            var slow = 0;
+            var fast = 0;
+
+            while (fast < nums.Length)
+            {
+                if (nums[fast] != 0)
+                {
+                    (nums[slow], nums[fast]) = (nums[fast], nums[slow]);
+                    slow++;
+                }
+
+                fast++;
+            }
         }
     }
 }
