10f7b2d5c9730d95c1d2fd914b073fa74dd8190c
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-04-25 18:27:50 +0800

    Done with two pointers.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace LeetCodeMaster
+﻿using System.Text;
+
+namespace LeetCodeMaster
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var s = "Test1ng-Leet=code-Q!";
+            Console.WriteLine(ReverseOnlyLetters(s));
+        }
+
+        static string ReverseOnlyLetters(string s)
+        {
+            var res = s.ToCharArray();
+            var left = 0;
+            var right = s.Length - 1;
+            while (left <= right)
+            {
+                if (!char.IsLetter(s[left])) left++;
+                else if (!char.IsLetter(s[right])) right--;
+                else
+                {
+                    (res[left], res[right]) = (s[right], s[left]);
+                    left++;
+                    right--;
+                }
+            }
+
+            return new string(res);
         }
     }
 }
