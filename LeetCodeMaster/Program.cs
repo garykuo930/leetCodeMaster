24dee5b373ff5a58754c4bf712450165f9d6db19
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-04-25 19:14:00 +0800

    Done with two pointers.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string s = "Hello, World!";
+            Console.WriteLine(ReversePrefix(s, 'o'));
+        }
+
+        static string ReversePrefix(string word, char ch)
+        {
+            var c = word.ToCharArray();
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (c[i] == ch)
+                {
+                    var left = 0;
+                    var right = i;
+                    while (left <= right)
+                    {
+                        (c[left], c[right]) = (c[right], c[left]);
+                        left++;
+                        right--;
+                    }
+
+                    return new string(c);
+                }
+            }
+
+            return word;
         }
     }
 }
