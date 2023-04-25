9c4cddc8025cf250bfcf6b4bdd363c1708b958aa
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-04-25 16:25:15 +0800

    Done with prefix sum.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var nums = new []{ 10, 4, -8, 7 };
+            Console.WriteLine(WaysToSplitArray(nums));
+        }
+        
+        static int WaysToSplitArray(int[] nums) {
+            //create prefix sum
+            var prefix = new long[nums.Length];
+            prefix[0] = nums[0];
+            for (var i = 1; i < nums.Length; i++)
+            {
+                prefix[i] = prefix[i - 1] + nums[i];
+            }
+
+            //find the answer
+            var ans = 0;
+            for (var i = 0; i < nums.Length - 1; i++)
+            {
+                var left = prefix[i];
+                var right = prefix[^1] - left;
+                if (left >= right) ans++;
+            }
+
+            return ans;
         }
     }
 }
