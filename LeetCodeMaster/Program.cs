03224f34a531de3778f824982cf8150099532d95
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-04-25 17:26:46 +0800

    Done with two pointers.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string s = "Let's take LeetCode contest";
+            var res = string.Join(" ", s.Split(" ").Select(w => Reverse(w)));
+            Console.WriteLine(res);
+        }
+        
+        static string Reverse(string s)
+        {
+            var res = new char[s.Length];
+            var left = 0;
+            var right = s.Length - 1;
+            while (left <= right)
+            {
+                (res[left], res[right]) = (s[right], s[left]);
+                left++;
+                right--;
+            }
+            return string.Join("", res);
         }
     }
 }
