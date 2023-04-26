f7fe9c466c6b0f59848e07a40d1d9ffb1eea61a9
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-04-26 17:30:06 +0800

    Done with sliding window.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(EqualSubstring("abcd", "cdef", 3));
+        }
+        
+        static int EqualSubstring(string s, string t, int maxCost)
+        {
+            var start = 0;
+            var end = 0;
+            var cost = 0;
+        
+            while (end < s.Length)
+            {
+                cost += Math.Abs(s[end] - t[end]);
+                if (cost > maxCost)
+                {
+                    cost -= Math.Abs(s[start] - t[start]);
+                    start++;
+                }
+                end++;
+            }
+
+            return end - start;
         }
     }
 }
