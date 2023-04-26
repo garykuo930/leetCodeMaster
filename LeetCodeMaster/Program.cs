baf60fdada13648c158df8f35753dacc12beaf11
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-04-26 15:44:25 +0800

    Done with sliding window.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var nums = new int[] { 1, 4, 4 };
+            Console.WriteLine(MinSubArrayLen(4,nums));
+        }
+        
+        static int MinSubArrayLen(int target, int[] nums) {
+            var res = 0;
+            var sum = 0;
+            var left = 0;
+
+            for (int right = 0; right < nums.Length; right++)
+            {
+                sum += nums[right];
+                while (left <= right && sum >= target)
+                {
+                    if (left == 0) res = right + 1;
+                    sum -= nums[left];
+                    res = Math.Min(res, right - left + 1);
+                    left++;
+                }
+            }
+
+            return res;
         }
     }
 }
