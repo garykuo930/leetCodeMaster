c67ed88f4719bca40d6394c1abdf4542a5576eef
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-04-26 16:12:00 +0800

    Done with fixed size sliding window.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -2,9 +2,35 @@
 {
     internal class Program
     {
+        static readonly char[] Lookup = new[] { 'a', 'e', 'i', 'o', 'u' };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(MaxVowels("ibpbhixfiouhdljnjfflpapptrxgcomvnb", 33));
+        }
+
+        static int MaxVowels(string s, int k)
+        {
+            var curr = 0;
+
+            for (var i = 0; i < k; i++)
+            {
+                curr += VowelCount(s[i]);
+            }
+            
+            var res = curr;
+            for (var right = k; right < s.Length; right++)
+            {
+                curr += VowelCount(s[right]) - VowelCount(s[right - k]);
+                res = Math.Max(res, curr);
+            }
+
+            return res;
+        }
+
+        static int VowelCount(char c)
+        {
+            return Lookup.Contains(c) ? 1 : 0;
         }
     }
 }
