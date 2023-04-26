cff79743cc610880ef15f6b9bb1dd793d67a690d
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-04-26 17:48:33 +0800

    Done with prefix sum.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var gain = new int[]{-4,-3,-2,-1,4,3,2};
+            Console.WriteLine(LargestAltitude(gain));
+        }
+
+        static int LargestAltitude(int[] gain)
+        {
+            //We can use prefix sum to find the largest point.
+            var prefix = new int[gain.Length];
+            prefix[0] = gain[0];
+            for (var i = 1; i < gain.Length; i++)
+            {
+                prefix[i] = prefix[i - 1] + gain[i];
+            }
+
+            return Math.Max(prefix.Max(), 0);
         }
     }
 }
