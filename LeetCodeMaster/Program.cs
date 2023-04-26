3c32e62a321eb15cacfb14395c9ba79de4651c55
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-04-26 18:28:20 +0800

    Done with prefix sum.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var nums = new int[] { -1, -1, 1, 1, 0, 0 };
+            Console.WriteLine(PivotIndex(nums));
+        }
+
+        static int PivotIndex(int[] nums)
+        {
+            var prefix = new int[nums.Length + 1];
+            prefix[0] = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                prefix[i + 1] = prefix[i] + nums[i];
+            }
+
+            var total = prefix[^1];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (total - nums[i] == prefix[i] * 2) return i;
+            }
+
+            return -1;
         }
     }
 }
