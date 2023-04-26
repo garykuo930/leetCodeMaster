81a5c2c8f1fc30feeeb0396ca66a7111bdcde6be
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-04-26 18:49:26 +0800

    Done with prefix sum.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var nums = new[] { -2, 0, 3, -5, 2, -1 };
+            var arr = new NumArray(nums);
+            Console.WriteLine(arr.SumRange(2, 5));
+        }
+    }
+    
+    public class NumArray
+    {
+        private readonly int[] _source;
+        private readonly int[] _prefixSum;
+        public NumArray(int[] nums)
+        {
+            _source = nums;
+            _prefixSum = new int[nums.Length];
+            _prefixSum[0] = nums[0];
+            for (var i = 1; i < nums.Length; i++)
+            {
+                _prefixSum[i] = _prefixSum[i - 1] + nums[i];
+            }
+        }
+    
+        public int SumRange(int left, int right)
+        {
+            return _prefixSum[right] - _prefixSum[left] + _source[left];
         }
     }
 }
