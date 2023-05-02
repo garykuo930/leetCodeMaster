7783f559f3f0a989906c2170cb62e76af8ebd801
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-05-02 17:02:46 +0800

    Done with hashmap.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace LeetCodeMaster
+﻿using System.Collections;
+
+namespace LeetCodeMaster
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+
+        static IList<int> Intersection(int[][] nums)
+        {
+            var counts = new Dictionary<int, int>();
+            foreach (var subNums in nums)
+            {
+                foreach (var num in subNums)
+                {
+                    if (counts.ContainsKey(num))
+                    {
+                        counts[num]++;
+                    }
+                    else
+                    {
+                        counts.Add(num, 1);
+                    }
+                }
+            }
+
+            var ans =
+                counts.Where(c => c.Value == nums.Length)
+                    .Select(c => c.Key)
+                    .OrderBy(n => n)
+                    .ToList();
+
+            return ans;
         }
     }
 }
