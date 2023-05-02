01b2aac7e8756ca770576385704c2915e5c57611
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-05-02 19:01:00 +0800

    Done with sliding window and hashmap.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -6,5 +6,22 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        
+        static int NumberOfSubarrays(int[] nums, int k)
+        {
+            var counts = new Dictionary<int, int>() { { 0, 1 } };
+            int curr = 0, ans = 0;
+
+            foreach (var num in nums)
+            {
+                curr += num % 2;
+                var prefix = curr - k;
+                if (counts.TryGetValue(prefix, out var count)) ans += count;
+                counts.TryAdd(curr, 0);
+                counts[curr]++;
+            }
+
+            return ans;
+        }
     }
 }
