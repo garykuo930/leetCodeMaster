c3129db373c6e3bacd3365322cf45f812ee0a1a8
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-05-02 18:38:32 +0800

    Done with sliding window and hashmap.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -6,5 +6,24 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        
+        static int SubarraySum(int[] nums, int k)
+        {
+            var counts = new Dictionary<int, int>();
+            var curr = 0;
+            var ans = 0;
+            
+            counts[0] = 1; //The empty array [] is a subarray with sum 0;
+            
+            for (var i = 0; i < nums.Length; i++)
+            {
+                curr += nums[i];
+                if (counts.ContainsKey(curr - k)) ans += counts[curr - k];
+                counts.TryAdd(curr, 0);
+                counts[curr]++;
+            }
+
+            return ans;
+        }
     }
 }
