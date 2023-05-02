c3a8363d147001bf4fc7970c7f6f70a53f918c10
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-05-02 18:23:17 +0800

    Done with set.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var str = "aaabb";
+            Console.WriteLine(AreOccurrencesEqual(str));
+        }
+
+        static bool AreOccurrencesEqual(string s)
+        {
+            // var lookup = s.ToCharArray().GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+            var map = new Dictionary<char, int>();
+            foreach (var c in s)
+            {
+                map.TryAdd(c, 0);
+                map[c]++;
+            }
+            var set = new HashSet<int>(map.Values);
+            return set.Count == 1;
         }
     }
 }
