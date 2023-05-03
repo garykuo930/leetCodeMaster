c3d4c930c3fb4aaa27ae798fd29e26818cd9d434
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-05-03 16:00:07 +0800

    Done with hashmap.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -6,5 +6,32 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        static IList<IList<int>> FindWinners(int[][] matches)
+        {
+            var losesCount = new Dictionary<int, int>();
+            foreach (var match in matches)
+            {
+                var winner = match[0];
+                var loser = match[1];
+                losesCount.TryAdd(winner, 0);
+                losesCount.TryAdd(loser, 0);
+                losesCount[loser]++;
+            }
+
+            var winners = new List<int>();
+            var loseOneGame = new List<int>();
+
+            foreach (var count in losesCount)
+            {
+                if (count.Value == 0) winners.Add(count.Key);
+                else if (count.Value == 1) loseOneGame.Add(count.Key);
+            }
+            
+            winners.Sort();
+            loseOneGame.Sort();
+            
+            return new List<IList<int>> { winners, loseOneGame };
+        }
     }
 }
