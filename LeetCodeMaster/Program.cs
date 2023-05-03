6f513393c35290a7fe65811a62ece0532fe87877
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-05-03 17:06:21 +0800

    Done with hashmap.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -6,5 +6,13 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        public int MaxNumberOfBalloons(string text)
+        {
+            var target = "balloon";
+            var requires = target.ToCharArray().GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            var counts = text.ToCharArray().Where(x => requires.ContainsKey(x)).GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            return requires.Min(x => counts.TryGetValue(x.Key, out var count) ? count / x.Value : 0);
+        }
     }
 }
