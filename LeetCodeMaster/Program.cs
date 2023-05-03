3092ede7e15785923b0fea4126c5babf37a87ae1
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-05-03 18:21:53 +0800

    Done with hashmap.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -6,5 +6,26 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        public int MinimumCardPickup(int[] cards)
+        {
+            var map = new Dictionary<int, int>();
+            var ans = int.MaxValue;
+            for (int i = 0; i < cards.Length; i++)
+            {
+                var card = cards[i];
+                if (map.ContainsKey(card))
+                {
+                    ans = Math.Min(ans, i - map[card] + 1);
+                    map[card] = i;
+                }
+                else
+                {
+                    map.Add(card, i);
+                }
+            }
+
+            return ans == int.MaxValue ? -1 : ans;
+        }
     }
 }
