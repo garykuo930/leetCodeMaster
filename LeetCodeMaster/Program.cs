53b58838697703e4c7acce6e89882c1d8ce021c9
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-05-03 20:21:39 +0800

    Done with hashmap.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -6,5 +6,23 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            if (magazine.Length < ransomNote.Length) return false;
+            
+            var source = magazine.ToCharArray().GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            foreach (var c in ransomNote)
+            {
+                if (!source.ContainsKey(c) || source[c] == 0)
+                {
+                    return false;
+                }
+
+                source[c]--;
+            }
+
+            return true;
+        }
     }
 }
