e949480f18a4c08062519c8be49572c4e57a602d
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-05-03 21:14:57 +0800

    Done with hashmap.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -6,5 +6,38 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        public int MaximumSum(int[] nums)
+        {
+            var map = new Dictionary<int, int>();
+            var sum = 0;
+            foreach (var num in nums)
+            {
+                var key = GetDigitSum(num);
+                if (map.ContainsKey(key))
+                {
+                    sum = Math.Max(sum, map[key] + num);
+                    map[key] = Math.Max(map[key], num);
+                }
+                else
+                {
+                    map.Add(key, num);
+                }
+            }
+
+            return sum;
+        }
+
+        public int GetDigitSum(int num)
+        {
+            var sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            return sum;
+        }
     }
 }
