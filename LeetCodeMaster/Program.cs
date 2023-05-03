08cf5aa24ce8241b5118d3b10944b25b5990d43a
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-05-03 22:05:15 +0800

    Done with 2 hashmaps.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -6,5 +6,29 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        public int EqualPairs(int[][] grid)
+        {
+            var rowMap = grid.GroupBy(ParseToString).ToDictionary(x => x.Key, x => x.Count());
+            var colMap = new int[grid[0].Length].Select((_, i) => GetColumn(grid, i)).GroupBy(ParseToString).ToDictionary(x => x.Key, x => x.Count());
+            var answer = rowMap.Sum(row => colMap.TryGetValue(row.Key, out var value) ? row.Value * value : 0);
+            return answer;
+        }
+
+        private static int[] GetColumn(int[][] grid, int col)
+        {
+            var curr = new int[grid.Length];
+            for (int row = 0; row < grid.Length; row++)
+            {
+                curr[row] = grid[row][col];
+            }
+
+            return curr;
+        }
+
+        private static string ParseToString(int[] nums)
+        {
+            return string.Join(",", nums);
+        }
     }
 }
