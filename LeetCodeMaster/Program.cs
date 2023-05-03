fe7cd3a0e3ac4e810a911738cceba30c6491e66a
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-05-03 16:07:40 +0800

    Done with hashset.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -6,5 +6,21 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        
+        static int LargestUniqueNumber(int[] nums) {
+            var seen = new HashSet<int>();
+            var candidates = new List<int>();        
+        
+            foreach(var num in nums){            
+                if(seen.Contains(num)){
+                    candidates.Remove(num);
+                }else{
+                    candidates.Add(num);
+                }
+                seen.Add(num);            
+            }
+        
+            return candidates.Any() ? candidates.Max() : -1;        
+        }
     }
 }
