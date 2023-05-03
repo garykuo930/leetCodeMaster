f4d7bc5a792a7b8c92464349b00a2424f5bb56f0
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-05-03 16:06:23 +0800

    Done with hashset.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -6,5 +6,15 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        
+        static List<int>[] FindDifference(int[] nums1, int[] nums2)
+        {
+            var set1 = new HashSet<int>(nums1);
+            var set2 = new HashSet<int>(nums2);
+            var ans1 = set1.Where(n => !set2.Contains(n)).ToList();
+            var ans2 = set2.Where(n => !set1.Contains(n)).ToList();
+
+            return new[] { ans1, ans2 };
+        }
     }
 }
