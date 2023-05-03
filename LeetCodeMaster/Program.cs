70b21b239e9209af8930f9ed8a4cf76b22983d06
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-05-03 17:54:15 +0800

    Done with hashmap.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -6,5 +6,26 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var map = new Dictionary<string, IList<string>>();
+            foreach (var str in strs)
+            {
+                var chars = str.ToCharArray();
+                Array.Sort(chars);
+                var key = new string(chars);
+                if (map.ContainsKey(key))
+                {
+                    map[key].Add(str);
+                }
+                else
+                {
+                    map.Add(key, new List<string> {str});
+                }
+            }
+
+            return map.Values.ToList();
+        }
     }
 }
