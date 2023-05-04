e5f2c9f542aab71dbd4bc166bd4dc0a71646f433
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-05-04 15:06:14 +0800

    Done with hashset.

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -6,5 +6,14 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        
+        public int NumJewelsInStones(string jewels, string stones) {
+            var set = new HashSet<char>(jewels.ToCharArray());
+            var sum = 0;
+            foreach(var s in stones){
+                if(set.Contains(s)) sum++;
+            }
+            return sum;
+        }
     }
 }
