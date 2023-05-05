30eb96f59cdc6dcc373ad36f735f9c73f21d8ba5
郭庭彰 <ji3g4vul3eji@gmail.com>
2023-05-05 14:35:53 +0800

    Add stopwatch

--- LeetCodeMaster/Program.cs
+++ LeetCodeMaster/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace LeetCodeMaster
+﻿using System.Diagnostics;
+
+namespace LeetCodeMaster
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            // 建立Stopwatch物件
+            Stopwatch stopwatch = new Stopwatch();
+
+            // 開始計時
+            stopwatch.Start();
+
+            // 要評估執行時間的function
+            MyFunction();
+
+            // 停止計時
+            stopwatch.Stop();
+
+            // 輸出執行時間
+            Console.WriteLine("MyFunction 執行時間：{0}", stopwatch.Elapsed);
+        }
+
+        static void MyFunction()
+        {
+            // 在此撰寫你要評估執行時間的function
+            for (int i = 0; i < 100000000; i++)
+            {
+                // do something
+            }
         }
     }
 }
